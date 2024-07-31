8aba341e26f24ad69b8698fa5ff8d0d1e3da7669
Nabilkencana22 <mohammad_kencana_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:51:37 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("Nama : MOHAMMAD NABIL ANWAR KENCANA");
+        System.out.println("Alamat : Dan. Alfatah rt/two 003/002 desa gendingan kec. Kedungwaru kabupaten tulungagung");
+        System.out.println("SMP : Mtsn 6 Tulungagung");
+        System.out.println("No.HP : 085648898807");
+    }
+}
